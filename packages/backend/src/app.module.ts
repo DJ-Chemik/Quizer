be1c770d2324380bfbbcdebc9b3ff59fc5820319
Patryk Jakubowski <jakubowski.patryk970223@gmail.com>
2023-04-13 16:18:41 +0200

    [Q-15] Added env files for all packages

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module, CacheModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import type { RedisClientOptions } from 'redis';
 import { redisStore } from 'cache-manager-redis-store';
 import { AppController } from './app.controller';
@@ -8,11 +9,14 @@ import { ExampleModule } from './Example/example.module';
 @Module({
   imports: [
     ExampleModule,
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'production' ? '.env' : '.env.local',
+    }),
     CacheModule.register<RedisClientOptions>({
       isGlobal: true,
       store: redisStore,
-      host: 'localhost',
-      port: 6379,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
     }),
   ],
   controllers: [AppController],
