7c7ba38f53bac47746ef54b73f326efbdd002539
bocian1337 <48670422+bocian1337@users.noreply.github.com>
2023-04-18 23:06:21 +0200

    [Q-10] Check linter yml

--- packages/admin-page/src/pages/404.tsx
+++ packages/admin-page/src/pages/404.tsx
@@ -23,7 +23,7 @@ const codeStyles = {
   borderRadius: 4,
 };
 
-const NotFoundPage: React.FC<PageProps> = () => {
+const NotFoundPage: React.FC = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>Page not found</h1>
