245d7ae3250da99df1c67d39c27a7538a60155a9
bocian1337 <48670422+bocian1337@users.noreply.github.com>
2023-04-18 23:14:57 +0200

    [Q-10] Eslint workflow checked ok

--- packages/admin-page/src/pages/404.tsx
+++ packages/admin-page/src/pages/404.tsx
@@ -23,7 +23,7 @@ const codeStyles = {
   borderRadius: 4,
 };
 
-const NotFoundPage: React.FC = () => {
+const NotFoundPage: React.FC<PageProps> = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>Page not found</h1>
