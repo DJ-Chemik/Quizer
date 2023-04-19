c4caa80ac3403e7ad3fbc2aadc32b84ae9cffedf
DJ-Chemik <dj-chemik@wp.pl>
2023-04-19 23:22:57 +0200

    Add Dummy Component to make request in frontend app

--- packages/frontend/src/QuizerRoot/QuizerRoot.tsx
+++ packages/frontend/src/QuizerRoot/QuizerRoot.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import DummyBackendConnector from '../DummyBackendConnector/DummyBackendConnector';
 import { QuizerRootWrapper } from './QuizerRoot.styled';
 
 const QuizerRoot = () => {
   return (
     <QuizerRootWrapper>
       <main>Quizer Root</main>
+      <DummyBackendConnector />
     </QuizerRootWrapper>
   );
 };
