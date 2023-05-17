e71d29e59726119e7b8865ff16ff739426ac55c6
Chemik <dj-chemik@wp.pl>
2023-05-17 22:28:21 +0200

    Add api for categories

---
+++ packages/backend/src/Category/category.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  CacheInterceptor,
+  CacheKey,
+  CacheTTL,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CategoryInput, CategoryUpdateInput } from '@shared/interfaces';
+
+@Controller('/categories')
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) { }
+
+  @UseInterceptors(CacheInterceptor)
+  @Get()
+  getCategories() {
+    return this.categoryService.getCategories();
+  }
+
+  @Get(':id')
+  getCategory(@Param('id') id: string) {
+    return this.categoryService.getCategory(id);
+  }
+
+  @Post()
+  createQuestion(@Body() input: CategoryInput) {
+    return this.categoryService.createCategory(input);
+  }
+
+  @Post(':id')
+  updateCategory(@Param('id') id: string, @Body() input: CategoryUpdateInput) {
+    return this.categoryService.updateCategory(id, input);
+  }
+
+  @Delete(':id')
+  deleteQuestion(@Param('id') id: string) {
+    return this.categoryService.deleteCategory(id);
+  }
+}
---
+++ packages/backend/src/Category/category.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CategoryInput, CategoryUpdateInput } from '@shared/interfaces';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class CategoryService {
+  constructor(private prisma: PrismaService) { }
+
+  async getCategories() {
+    return await this.prisma.category.findMany();
+  }
+
+  async getCategory(id: string) {
+    return await this.prisma.category.findFirst({ where: { id }});
+  }
+
+  async createCategory(input: CategoryInput) {
+    return await this.prisma.category.create({
+      data: {
+        name: input.name,
+        creator: { connect: { id: input.creatorId }},
+      },
+    });
+  }
+
+  async updateCategory(id: string, input: CategoryUpdateInput) {
+    return await this.prisma.category.update({
+      where: { id },
+      data: {
+        name: input.name,
+      },
+    });
+  }
+
+  async deleteCategory(id: string) {
+    return await this.prisma.category.delete({ where: { id }});
+  }
+}
