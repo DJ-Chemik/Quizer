40f20ac8991f34dd2316a3a71c3e2a7f40513a7b
Chemik <dj-chemik@wp.pl>
2023-07-06 21:37:25 +0200

    Create endpoint and authentication guard

--- packages/backend/src/app.controller.ts
+++ packages/backend/src/app.controller.ts
@@ -1,9 +1,16 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Request, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
 
   @Get()
   getHello(): string {
