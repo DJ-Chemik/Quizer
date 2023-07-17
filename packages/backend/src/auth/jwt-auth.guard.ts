04e165d5722eba1cce5dd9d3885a574cab31cad6
Chemik <dj-chemik@wp.pl>
2023-07-17 18:15:34 +0200

    Extend JWT guard

--- packages/backend/src/auth/jwt-auth.guard.ts
+++ packages/backend/src/auth/jwt-auth.guard.ts
@@ -1,5 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    // Custom authentication logic here
+    return super.canActivate(context);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  handleRequest<TUser = unknown>(err: unknown, user: TUser, info: unknown) {
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
+}
